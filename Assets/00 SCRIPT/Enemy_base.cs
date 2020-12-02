efffd805522aea465ea71a024d039b2efd6e2cc9
Nguyễn Nhật Huy <nhat.huy.7996@gmail.com>
2020-12-02 19:46:32 +0700

    enemy chase

--- Assets/00 SCRIPT/Enemy_base.cs
+++ Assets/00 SCRIPT/Enemy_base.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Enemy_base : MonoBehaviour
 {
-    [SerializeField] float HP = 100;
+    [SerializeField] float HP = 100, Atk_range = 10;
     [SerializeField] Vector2 Speed;
     GameObject Player;
-    float dir = 1;
+    [SerializeField]float dir = 1;
     Rigidbody2D Rigi;
     [SerializeField] Vector2 offset = new Vector2(0,0.5f);
     Collider2D Colli;
+    [SerializeField] bool IsChasePlayer = false;
+
     private void OnEnable()
     {
         HP = 100;
+        IsChasePlayer = false;
     }
     // Start is called before the first frame update
     void Start()
@@ -26,25 +29,57 @@ public class Enemy_base : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckTarget();
         Move();
+
     }    
 
     void Move()
     {
-        //dir = this.transform.position.x > Player.transform.position.x ? -1 : 1;
+        if(IsChasePlayer)
+            this.dir = this.transform.position.x > Player.transform.position.x ? -1 : 1;
+
+        if (!CheckIsGround(this.dir))
+        {
+            this.dir = -this.dir;
+        }
+
+        this.Rigi.velocity = new Vector2(this.dir * Speed.x * Time.deltaTime,this.Rigi.velocity.y);
+    }
+
+    bool CheckIsGround(float dir)
+    {
         offset.x = -dir;
-        Debug.DrawRay(this.transform.position - (Vector3)offset,Vector2.down,Color.red);
-        RaycastHit2D hit =  Physics2D.Raycast(this.transform.position - (Vector3)offset, Vector2.down, 1);
+        Debug.DrawRay(this.transform.position - (Vector3)offset, Vector2.down, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position - (Vector3)offset, Vector2.down, 1);
         if (hit.collider == null)
         {
-            dir = -dir;
+            return false;
         }
         else
         {
-            Debug.Log(hit.collider.name);
+            return true;
         }
+    }
 
-        this.Rigi.velocity = new Vector2(dir * Speed.x * Time.deltaTime,this.Rigi.velocity.y);
+    void CheckTarget()
+    {
+        if (Vector2.Distance(Player.transform.position, this.transform.position) > Atk_range)
+        {
+            IsChasePlayer = false;
+            return;
+        }
+        float dir = this.transform.position.x > Player.transform.position.x ? -1 : 1;
+        Debug.Log(dir + "||" + CheckIsGround(dir));
+        if (CheckIsGround(dir))
+        {
+            IsChasePlayer = true;
+        }
+        else
+        {
+            IsChasePlayer = false;
+        }
+       
     }
 
     public void GetDamage(Bullet_base B)
@@ -56,5 +91,16 @@ public class Enemy_base : MonoBehaviour
             //Dead
             this.gameObject.SetActive(false);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(this.transform.position, Atk_range);
     }
 }
